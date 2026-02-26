9297b5df623c6ae0673638c270211a0cf8cb3be5
noraarohii <aarohibhakuni022106@gmail.com>
2026-02-26 15:35:53 +0530

    Aarohi added use case 2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,28 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management System");
+
+    public static void main(String[] args) {
+
+        String text = "madam";
+
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string \"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + text + "\" is NOT a palindrome.");
+        }
+
     }
 }
